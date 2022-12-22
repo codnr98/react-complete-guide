3322521fb7ce3cce1cc03424315476db9c086116
codnr <wnd75913@gmail.com>
2022-12-23 00:37:07 +0900

    refactor: 컴포넌트 및 함수를 화살표 함수로 변경

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,11 @@
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
   const classes = "card " + props.className;
   // 위 코드를 통해 className에 "card props로_받는_className"를 담을 수 있다.
 
   return <div className={classes}>{props.children}</div>;
   // {props.children}을 적용해야 해당 div 태그 안에 다른 태그를 담을 수 있다.
-}
+};
 
 export default Card;
